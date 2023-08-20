a8f49ba9edd278e330b9c442dbe76302bbc3b3f3
lope1999 <corps.technical2.union@gmail.com>
2023-08-20 16:16:12 +0100

    Weather API Integration, Weather Date, Weather Search Engine, Weather Animated Icon

---
+++ src/FormatedDate.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function FormatedDate({ date }) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return (
+    <span>
+      Last Updated: {day} {hours}:{minutes},
+    </span>
+  );
+}
